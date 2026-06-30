b4b571e7c001d04134d437cc8ffe163770e99306
tawanda nyakudjga <tawanda.nyakudjga@gmail.com>
2026-06-30 12:14:06 +1000

    fix: update base path in Vite configuration for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/mathematical-equation-parser/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
